9b4719ffbea5c5ae438a513eaa0beffb5e22c6aa
Alejandro Exequiel Hernández Lara <alejandrehl@icloud.com>
2019-08-02 09:54:30 -0400

    Easy toast

--- App.js
+++ App.js
@@ -1,26 +1,58 @@
-import React from "react";
-import { StyleSheet, Button, View } from "react-native";
+import React, { useRef } from "react";
+import { StyleSheet, View, Text } from "react-native";
+import { Button } from "react-native-elements";
+import Icon from "react-native-vector-icons/FontAwesome";
+import * as Permissions from "expo-permissions";
+import Toast from "react-native-easy-toast";
 
 const App = () => {
-  const openCamera = () => console.log("Abrir Cámara");
+  const toast = useRef(null);
+
+  const openCamera = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    if (status === "denied") {
+      toast.current.show("Es necesario aceptar los permisos cámara");
+    } else {
+      toast.current.show("Permisos aceptados");
+    }
+  };
 
   return (
-    <View style={styles.container}>
+    <View style={styles.viewBody}>
+      <Text>Bar Code Info App</Text>
       <Button
         onPress={() => openCamera()}
-        title="Leer código de Barra"
-        color="#841584"
+        icon={
+          <Icon
+            name="camera"
+            size={15}
+            color="white"
+            style={styles.cameraIcon}
+          />
+        }
+        title="Leer código de barra"
+      />
+      <Toast
+        ref={toast}
+        position="bottom"
+        positionValue={250}
+        fadeInDuration={750}
+        fadeOutDuration={1000}
+        opacity={0.8}
+        textStyle={{ color: "#fff" }}
       />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
+  viewBody: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center"
+  },
+  cameraIcon: {
+    margin: 10
   }
 });
 
---
+++ components/overlays/Camera.js
@@ -0,0 +1,76 @@
+import React, { useRef } from "react";
+import { StyleSheet, View, Vibration, Linking } from "react-native";
+import { Overlay, Icon } from "react-native-elements";
+import { Camera } from "expo-camera";
+
+const Camera = ({ closeFunction, toast }) => {
+  const camera = useRef(null);
+
+  const onBarCodeScanned = ({ type, data }) => {
+    if (type !== "org.iso.QRCode") {
+      toast.show("Solo se permiten códigos QR");
+    } else {
+      Vibration.vibrate();
+      closeFunction();
+      toast.show("Abriendo navegador...", 1000, () => {
+        Linking.openURL(data);
+      });
+    }
+  };
+
+  return (
+    <Overlay
+      width="100%"
+      height="100%"
+      overlayBackgroundColor="transparent"
+      isVisible={true}
+      overlayStyle={styles.overlayStyle}
+    >
+      <View style={styles.viewCamera}>
+        <Camera
+          style={styles.camera}
+          ref={camera}
+          type={Camera.Constants.Type.back}
+          onBarCodeScanned={onBarCodeScanned}
+        />
+        <Icon
+          containerStyle={styles.containerIconClose}
+          type="material-community"
+          name="close-circle-outline"
+          size={30}
+          color="#ffc107"
+          onPress={closeFunction}
+        />
+      </View>
+    </Overlay>
+  );
+};
+
+const styles = StyleSheet.create({
+  overlayStyle: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  viewCamera: {
+    width: "100%",
+    height: "50%",
+    borderColor: "#ffc107",
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2
+  },
+  camera: {
+    flex: 1,
+    width: "auto",
+    height: "auto"
+  },
+  containerIconClose: {
+    position: "absolute",
+    right: -15,
+    top: -16
+  }
+});
+
+export default Camera;
